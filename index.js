31e50f5416cd68bd34ac38e9bc467d348e4fc256
Mohsin raza <mohsin552001@gmail.com>
2022-08-15 20:28:14 +0500

    some responsive changes i have created at this position

--- index.js
+++ index.js
@@ -7,33 +7,21 @@ function shwoproducts() {
     } else {
         product.style.display = "none";
     }
+
 }
 
 
 function signinbtns() {
-    let product = document.querySelector('.bottomheaderSection .signupbtns .btns')
+    let btns = document.querySelector('.bottomheaderSection .signupbtns .btns')
 
-    if (product.style.display === "none") {
-        product.style.display = "flex";
+    if (btns.style.display === "none") {
+        btns.style.display = "flex";
 
     } else {
-        product.style.display = "none";
+        btns.style.display = "none";
 
     }
 }
 
 
 
-function show() {
-    let product = document.querySelector('.bottomheaderSection .visibleproducts')
-
-    let header = document.querySelector('.bottomheaderSection .bottomheader')
-    product.style.display = "block"
-}
-function hide() {
-    let product = document.querySelector('.bottomheaderSection .visibleproducts')
-
-
-    product.style.display = "none"
-
-}
